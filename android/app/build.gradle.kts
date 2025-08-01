10cefe1236bc9b9ed5862e69b6204bf89e24f650
Nahid Khan Shuvo <nahidkhanshuvo7@gmail.com>
2025-08-02 01:22:22 +0600

    ndk version changed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 android {
     namespace = "com.example.ist_blood_donors"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
