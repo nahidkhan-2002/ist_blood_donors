d5d783eb7f8942296b861242f1df26d34c7299f3
Nahid Khan Shuvo <nahidkhanshuvo7@gmail.com>
2025-08-03 18:25:05 +0600

    changes in build.gradle changing min sdk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.example.ist_blood_donors"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
